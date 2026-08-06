b6615bc9c8f80cc342feb7ed50bf05032d4da38a
Ashwini Kauthale <kauthaleashwini40@gmail.com>
2026-08-06 14:48:35 +0530

    Fixed API URLs for production

--- src/api/batchApi.js
+++ src/api/batchApi.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.VITE_API_URL}/batches`
+const API_URL = `${import.meta.env.VITE_API_URL}/batches`;
 
+// Get all batches
 export const getBatches = () => axios.get(API_URL);
 
+// Get batch by ID
 export const getBatchById = (id) =>
-  axios.get(`${API}/id/${id}`);
+  axios.get(`${API_URL}/id/${id}`);
 
+// Create batch
 export const createBatch = (batch) =>
-  axios.post(API, batch);
+  axios.post(API_URL, batch);
 
+// Update batch
 export const updateBatch = (id, batch) =>
-  axios.put(`${API}/id/${id}`, batch);
+  axios.put(`${API_URL}/id/${id}`, batch);
 
+// Delete batch
 export const deleteBatch = (id) =>
-  axios.delete(`${API}/id/${id}`);
+  axios.delete(`${API_URL}/id/${id}`);
